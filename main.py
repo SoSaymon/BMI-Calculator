c13899f6986fc761a5d77351698a3b578035218c
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-07 00:26:17 +0200

    First working version, only metric

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+def calculate_square(height):
+    return height * height
+
+
+def calculate_bmi_metric(height, weight):
+    height_square = calculate_square(height)
+    return float(weight / height_square)
+
+
 def main():
-    pass
+    print("Hi, welcome in BMI calculator!")
+    height = float(input("Please tell me your height in m\nExample input: 1.83\n"))
+    weight = float(input("Please tell me your weight in kg\nExample input: 70.5\n"))
+    bmi = calculate_bmi_metric(height, weight)
+    print(f"Your BMI is {bmi}")
+    return 0
+
 
 if __name__ == '__main__':
     main()
